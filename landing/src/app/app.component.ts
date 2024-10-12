3a97ce15f93ca9f0fcfeff56569a49e1c9830e6a
Lucas Jesus <lucassj.dev@gmail.com>
2024-10-12 09:10:24 -0300

    feat: Ajustes de layout

--- landing/src/app/app.component.ts
+++ landing/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
 import { TrajetoriaComponent } from './trajetoria/trajetoria.component';
+import { ContratarComponent } from './contratar/contratar.component';
+import { ServicosComponent } from './servicos-oferecidos/servicos.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, SobreMimComponent, TrajetoriaComponent],
+  imports: [RouterModule, HeaderComponent, SobreMimComponent, TrajetoriaComponent, ContratarComponent, ServicosComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
