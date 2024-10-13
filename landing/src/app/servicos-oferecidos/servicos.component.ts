0554c3bc20e53d737d3b1eb7d4b3b56d13c6c3e4
Lucas Jesus <lucassj.dev@gmail.com>
2024-10-13 09:03:55 -0300

    feat: Include caroussel

--- landing/src/app/servicos-oferecidos/servicos.component.ts
+++ landing/src/app/servicos-oferecidos/servicos.component.ts
@@ -12,6 +12,43 @@ import { CardImageComponent } from '../shared/card-image/card-image.component';
 export class ServicosComponent {
   protected  isFirstMain = signal<boolean>(true);
 
+  listimoveis: any[] = [
+    {
+      image: 'https://giassiferroeaco.com.br/wp-content/uploads/2019/01/mao-de-obra-sem.png',
+      name: 'Vistoria de Imóveis 0',
+      tags: ['tag1', 'tag2', 'tag3'],
+      description: 'Descrição do serviço',
+    },
+    {
+      image: 'https://giassiferroeaco.com.br/wp-content/uploads/2019/01/mao-de-obra-sem.png',
+      name: 'Vistoria de Imóveis 1',
+      link: '',
+      tags: ['tag1', 'tag2', 'tag3'],
+      description: 'Descrição do serviço',
+    },
+    {
+      image: 'https://giassiferroeaco.com.br/wp-content/uploads/2019/01/mao-de-obra-sem.png',
+      name: 'Vistoria de Imóveis 2',
+      link: '',
+      tags: ['tag1', 'tag2', 'tag3'],
+      description: 'Descrição do serviço',
+    },
+    {
+      image: 'https://giassiferroeaco.com.br/wp-content/uploads/2019/01/mao-de-obra-sem.png',
+      name: 'Vistoria de Imóveis 3',
+      link: '',
+      tags: ['tag1', 'tag2', 'tag3'],
+      description: 'Descrição do serviço',
+    },
+    {
+      image: 'https://giassiferroeaco.com.br/wp-content/uploads/2019/01/mao-de-obra-sem.png',
+      name: 'Vistoria de Imóveis 4',
+      link: '',
+      tags: ['tag1', 'tag2', 'tag3'],
+      description: 'Descrição do serviço',
+    },
+  ];
+
   changeMain(value: boolean) {
     this.isFirstMain.set(value);
   }
--- landing/src/app/shared/card-image/card-image.component.ts
+++ landing/src/app/shared/card-image/card-image.component.ts
@@ -1,15 +1,43 @@
-import { Component, input, InputSignal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, input, InputSignal, OnInit, ViewEncapsulation} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-card-image',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CarouselModule, NgOptimizedImage],
   templateUrl: './card-image.component.html',
   styleUrl: './card-image.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class CardImageComponent {
-  imagePath: InputSignal<unknown> = input.required({
-    alias: 'imagePath'
+export class CardImageComponent implements OnInit {
+  listItems: InputSignal<Array<any>> = input.required({
+    alias: 'listItems'
   });
+  responsiveOptions: any[] | undefined;
+
+  ngOnInit() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
+
+  goToLink() {
+    //TODO: Ajustar aqui
+
+  }
 }
